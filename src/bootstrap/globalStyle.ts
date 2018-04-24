2f4b5055af2100edde712588b781bf5cf3e815c8
Braden Marshall <braden1996@hotmail.co.uk>
2018-04-24 23:58:36 +0100

    Inject global CSS to reset all elements.

---
+++ src/bootstrap/globalStyle.ts
@@ -0,0 +1,17 @@
+import { injectGlobal } from 'styled-components';
+import reset from 'styled-reset'
+
+export default function() {
+  // tslint:disable no-unused-expression
+  injectGlobal`
+    ${reset}
+
+    html {
+      box-sizing: border-box;
+    }
+
+    *, *:before, *:after {
+      box-sizing: inherit;
+    }
+  `;
+}
--- src/bootstrap/index.tsx
+++ src/bootstrap/index.tsx
@@ -6,9 +6,11 @@ import { theme } from '@theme';
 import store from '../store';
 import { history } from '../store';
 import App from './App';
+import injectGlobalStyle from './globalStyle';
 import registerServiceWorker from './registerServiceWorker';
 
 export default function bootstrap() {
+  injectGlobalStyle();
   ReactDOM.render(
     <App
       store={store}
