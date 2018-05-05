cd08fb6d543eb9658ddbe76524c35c7faa9a1d1c
Braden Marshall <braden1996@hotmail.co.uk>
2018-05-05 22:31:48 +0100

    Modify deep readonly type to work correctly for arrays.

--- src/typings/deep.d.ts
+++ src/typings/deep.d.ts
@@ -1,11 +1,11 @@
 declare namespace Deep {
   export type Readonly<T> =
-    T extends ReadonlyArray<infer U> ? ReadonlyArray<U> :
+    T extends ReadonlyArray<infer U> ? DeepReadonlyArray<U> :
     T extends Function ? T :
     T extends object ? ReadonlyObject<T> :
     T;
 
-  export interface ReadonlyArray2<T> extends ReadonlyArray<Readonly<T>> {}
+  export interface DeepReadonlyArray<T> extends ReadonlyArray<Readonly<T>> {}
 
   export type ReadonlyObject<T> = {
     readonly [P in keyof T]: Readonly<T[P]>;
